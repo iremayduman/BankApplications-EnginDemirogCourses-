540975dfaa54c2cb6532b3386de8cf4fee284807
iremcoder <77546520+iremcoder@users.noreply.github.com>
2021-02-09 14:03:16 +0300

    BankApplications(Engin Demiroğ Courses)
    Banka kredilerini hesaplama,veri tabanına ve dosyaya loglama

---
+++ OOP3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OOP3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IKrediManager ihtiyacKrediManager = new IhtiyacKrediManager();
+            IKrediManager tasitKrediManager = new TasitKrediManager();
+            IKrediManager konutKrediManager = new KonutKrediManager();
+
+            ILoggerService fileloggerService = new FileLoggerService();
+            ILoggerService databaseloggerService = new DatabaseLoggerService();
+            BasvuruManager basvuruManager = new BasvuruManager();
+            basvuruManager.BasvuruYap(ihtiyacKrediManager,new List<ILoggerService> {new DatabaseLoggerService(),new FileLoggerService() });
+            
+            List<IKrediManager> krediler = new List<IKrediManager>() {ihtiyacKrediManager,tasitKrediManager };
+
+            //basvuruManager.KrediOnBilgilendirmesiYap(krediler);
+        }
+    }
+}
